fb11b12f773c0b346fe9e097f57045007c1c247b
Anurag Singh <anuragsingh2313@gmail.com>
2025-01-05 03:04:27 +0530

    landing page completed (mobile)

--- frontend/animation.js
+++ frontend/animation.js
@@ -8,20 +8,20 @@ const scroll = new LocomotiveScroll.default({
 document.querySelector('.add').addEventListener('click', () => {
     const navAnimationTimeline = gsap.timeline();
     navAnimationTimeline.to('.pre_loader_1', {
-        left: '-00%',
+        left: '-4%',
         duration: 0.45,
         ease: "expoScale(10,2.5,power2.out)",
 
     });
     navAnimationTimeline.to('.pre_loader_2', {
-        left: '-00%',
+        left: '-4%',
         duration: 0.55,
         ease: "expoScale(10,2.5,power2.out)",
 
 
     }, "-=0.1");
     navAnimationTimeline.to('.nav_container', {
-        left: '-5%',
+        left: '-3%',
         duration: 1.4,
         ease: "power4.out",
     }, "-=0.2");
@@ -30,19 +30,44 @@ document.querySelector('.add').addEventListener('click', () => {
 document.querySelector('.close').addEventListener('click', () => {
     const navAnimationTimeline = gsap.timeline();
     navAnimationTimeline.to('.pre_loader_2', {
-        left: '100%',
+        left: '110%',
         duration: 0,
         ease: 'power2.out',
     });
     navAnimationTimeline.to('.pre_loader_1', {
-        left: '100%',
+        left: '110%',
         duration: 0,
         ease: 'power2.out',
     });
     navAnimationTimeline.to('.nav_container', {
-        left: '100%',
+        left: '110%',
         duration: 1.2,
         ease: "power4.in",
     });
 
 });
+
+document.querySelector('.explore_button').addEventListener("mouseenter", () => {
+    gsap.to('.first_arrow', {
+        ease: "power2.in",
+        transform: "translate(100%, -100%)"
+    })
+    gsap.to('.second_arrow', {
+        delay: 0.25,
+        ease: "power2.out",
+        transform: "translate(0%, -00%)"
+    })
+})
+
+document.querySelector('.explore_button').addEventListener("mouseleave", () => {
+    gsap.to('.second_arrow', {
+        transform: "translate(-100%, 100%)",
+        ease: "power2.in",
+    })
+
+    gsap.to('.first_arrow', {
+        delay: 0.25,
+        ease: "power2.out",
+        transform: "translate(00%, 00%)",
+    })
+})
